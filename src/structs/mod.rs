7cdeaba83a45401875637338e9de5ec526e257d3
nixxoq <nixxoq@outlook.com>
2025-05-23 19:59:05 +0300

    feat(structs): remove unused `deserialise_optional_url` function
    [skip ci]

--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -5,24 +5,3 @@ use serde::{Deserialize, Serialize};
 use url::Url;
 
 pub type Date = chrono::DateTime<chrono::Utc>;
-
-fn deserialise_optional_url<'de, D: serde::Deserializer<'de>>(
-    de: D,
-) -> Result<Option<Url>, D::Error> {
-    use serde::de::{Error, Unexpected};
-    use std::borrow::Cow;
-
-    let intermediate = <Option<Cow<'de, str>>>::deserialize(de)?;
-    match intermediate.as_deref() {
-        None | Some("") => Ok(None),
-        Some(s) => Url::parse(s).map_or_else(
-            |err| {
-                Err(Error::invalid_value(
-                    Unexpected::Str(s),
-                    &err.to_string().as_str(),
-                ))
-            },
-            |ok| Ok(Some(ok)),
-        ),
-    }
-}
