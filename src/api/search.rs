108c2bb301ed7d7629a06b4b3b8fd02a6840dfd9
nixxoq <nixxoq@outlook.com>
2025-05-25 12:42:18 +0300

    feat(search): add tests related with fetching project info from SearchHit

--- src/api/search.rs
+++ src/api/search.rs
@@ -133,4 +133,58 @@ mod tests {
         assert_eq!(title.ok_or(0), Ok(&String::from("xaeros-minimap")));
         Ok(())
     }
+
+    #[tokio::test]
+    async fn test_fetching_project_with_mut() -> Result<()> {
+        let api = ModrinthAPI::default();
+        let response = api
+            .extended_search(
+                "xaeros",
+                &Sort::Downloads,
+                None,
+                Some(ExtendedSearch {
+                    offset: None,
+                    facets: vec![vec![Facet::ProjectType(ProjectType::Mod)]],
+                }),
+            )
+            .await?;
+
+        let response = response.hits.first().unwrap();
+        let title = response.slug.as_ref();
+
+        assert!(title.is_some());
+        assert_eq!(title.ok_or(0), Ok(&String::from("xaeros-minimap")));
+
+        let hit = response.to_owned().fetch_project(&api).await?;
+
+        assert_eq!(hit.slug.as_ref().unwrap(), &String::from("xaeros-minimap"));
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_fetching_project_without_mut() -> Result<()> {
+        let api = ModrinthAPI::default();
+        let response = api
+            .extended_search(
+                "xaeros",
+                &Sort::Downloads,
+                None,
+                Some(ExtendedSearch {
+                    offset: None,
+                    facets: vec![vec![Facet::ProjectType(ProjectType::Mod)]],
+                }),
+            )
+            .await?;
+
+        let response = response.hits.first().unwrap();
+        let title = response.slug.as_ref();
+
+        assert!(title.is_some());
+        assert_eq!(title.ok_or(0), Ok(&String::from("xaeros-minimap")));
+
+        let hit = response.get_full_project(&api).await?;
+
+        assert_eq!(hit.slug, String::from("xaeros-minimap"));
+        Ok(())
+    }
 }
